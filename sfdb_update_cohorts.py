089ac11f9498d49706342294a32db33d896f848a
yifei-men <ymen@dnanexus.com>
2016-06-17 11:34:08 -0700

    WIP: script for aggregating frequency .db files for multiple cohorts

---
+++ sfdb_update_cohorts.py
@@ -0,0 +1,89 @@
+import argparse
+import dxpy
+import time
+import yaml
+
+CONFIG_DEFAULT = {
+    'archive_project': 'project-BxVXgF80VjVbZ1QfF0PKzFxk',
+    'contributors': {},
+    'working_project': 'project-BxVK4Q8081jfqjyXfxGyQVbV'
+}
+
+""" Parse command line arguments """
+def parse_args():
+
+    parser = argparse.ArgumentParser(description='This script syncs genotype frequency databases' +
+                                                 ' from data contributors of the SG Federated Database' +
+                                                 ' project to a common admin project.'
+                                     formatter_class=argparse.RawTextHelpFormatter)
+
+    requiredNamed = parser.add_argument_group("Required named arguments")
+
+    requiredNamed.add_argument('--config', '-c',
+                        help='Path to config YAML file',
+                        type=argparse.FileType('r'),
+                        required=True)
+
+    return parser.parse_args()
+
+""" Print error message to stderr, exiting with non-zero state if hdr
+type is 'Error'."""
+def print_error(hdr, msg):
+    sys.stderr.write("=={hdr}== {msg}\n".format(hdr=hdr, msg=msg))
+    if hdr == "Error":
+        exit(1)
+
+""" Parse the config YAML file """
+def read_config(config_file):
+    config = {}
+    user_config = yaml.load(config_file)
+    for k, default_v in CONFIG_DEFAULT:
+        v = user_config.get(k, default_v)
+        if not v:
+            print_error("Error", "Configuration value for {opt} is not specified".format(opt=k))
+        config[k] = v
+    return config
+
+def get_dx_auth_token():
+    """Parses dx_auth_token from the output of dx env
+    Exits with error message if dx env failed to execute
+    or the environment is not set with an auth token"""
+
+    try:
+        return dxpy.SECURITY_CONTEXT['auth_token']
+
+    except KeyError, e:
+        sys.exit("Could not parse auth_token in dxpy environment, ensure that you have logged in " +
+            "using an API token!\n{0}: {1}".format(e.errno, e.strerror))
+
+
+""" Archive files in the working SFDB project """
+def archive_current_files(config):
+
+    try:
+        working_proj = dxpy.bindings.DXProject(config['working_project'])
+    except dxpy.exceptions.DXError, e:
+        print_error("Error",
+                    "Cannot access working project given ({0}). {1}".format(project, str(e)))
+
+    try:
+        archive_proj = dxpy.bindings.DXProject(config['archive_project'])
+    except dxpy.exceptions.DXError, e:
+        print_error("Error",
+                    "Cannot access archival project given ({0}). {1}".format(project, str(e)))
+
+
+    timestamp_folder = time.strftime("/%Y-%m/%d-%H%M%S")
+
+    archive_proj.new_folder(timestamp_folder)
+    folders = working
+    working_proj.move_folder()
+
+""" Main entry point """
+def main():
+    args = parse_args()
+    config = read_config(args.config)
+    archive_current_files(config)
+
+if __name__ == "__main__":
+    main()
